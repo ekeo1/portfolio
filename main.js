ab20f2af903c73e66bf741c6d9e870fa3ea6e68e
Simon Tvrdy <simon.tvrdy@student.spsmb.cz>
2025-03-07 21:31:12 +0100

    added new things

--- main.js
+++ main.js
@@ -10,3 +10,35 @@ function openWeb3(){
     window.open("https://ekeo1.github.io/bento-grid-main/")
 }
 
+function openService1(){
+    if(document.getElementById("serviceInfo1").style.display==="none" || document.getElementById("serviceInfo1").style.display===""){
+        document.getElementById("serviceInfo1").style.display="block";
+    } else{
+        document.getElementById("serviceInfo1").style.display="none";
+    } 
+}
+
+function openService2(){
+    if(document.getElementById("serviceInfo2").style.display==="none" || document.getElementById("serviceInfo2").style.display===""){
+        document.getElementById("serviceInfo2").style.display="block";
+    } else{
+        document.getElementById("serviceInfo2").style.display="none";
+    } 
+}
+
+function openService3(){
+    if(document.getElementById("serviceInfo3").style.display==="none" || document.getElementById("serviceInfo3").style.display===""){
+        document.getElementById("serviceInfo3").style.display="block";
+    } else{
+        document.getElementById("serviceInfo3").style.display="none";
+    } 
+}
+
+function openService4(){
+    if(document.getElementById("serviceInfo4").style.display==="none" || document.getElementById("serviceInfo4").style.display===""){
+        document.getElementById("serviceInfo4").style.display="block";
+    } else{
+        document.getElementById("serviceInfo4").style.display="none";
+    } 
+}
+
