a02618f30e97399b51ba69c42cacdb933d298351
Simon Tvrdy <simon.tvrdy@student.spsmb.cz>
2025-03-07 20:24:05 +0100

    hello

---
+++ main.js
@@ -0,0 +1,12 @@
+function openWeb1(){
+    window.open("https://ekeo1.github.io/Interactive-rating-component/")
+}
+
+function openWeb2(){
+    window.open("https://ekeo1.github.io/FAQ-accordion/")
+}
+
+function openWeb3(){
+    window.open("https://ekeo1.github.io/bento-grid-main/")
+}
+
